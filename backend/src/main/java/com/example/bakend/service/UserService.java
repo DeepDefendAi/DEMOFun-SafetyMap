cd6575858531351821751c0861e9aae019c8e817
Bogdan Dumitrescu <bogdan.dumitrescu02@stud.acs.upb.ro>
2023-10-04 17:16:42 +0300

    backend simple call working

---
+++ backend/src/main/java/com/example/bakend/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.bakend.service;
+
+import com.example.bakend.api.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+    public User getUser() {
+        User user = new User();
+        user.setId(1);
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setUserName("johndoe");
+        user.setEmail("johndodo@gmail.com");
+        user.setPassword("password");
+        return user;
+    }
+}
