f2152322dd599e01dc8a6cb55264e4cf1c316a51
AndreiNoroc <noroc_andrei@yahoo.com>
2023-10-09 03:11:19 +0300

    frontend communicate with backend

--- FrontendSafetyFunMap/app/src/main/java/com/example/frontendsafetyfunmap/Home.kt
+++ FrontendSafetyFunMap/app/src/main/java/com/example/frontendsafetyfunmap/Home.kt
@@ -5,9 +5,22 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.await
+interface ApiService {
+    @GET("/posts/")
+    fun getAllPosts(): Call<List<PostModel>>
+}
 
 class Home: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,14 +34,24 @@ class Home: Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        val clickHereButton = view.findViewById<Button>(R.id.buttonClickHere)
         val txtViewHome = view.findViewById<TextView>(R.id.textViewHome)
 
-        clickHereButton?.setOnClickListener{
-            if (txtViewHome?.text?.equals("") == true) {
-                txtViewHome.text = "Text showed"
-            } else {
-                txtViewHome?.text = ""
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://192.168.100.20:8080")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = retrofit.create(ApiService::class.java)
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = apiService.getAllPosts().await()
+                if (response.isNotEmpty()) {
+                    txtViewHome.text = response[0].username
+                } else {
+                    // Handle an error response (non-2xx status code)
+                }
+            } catch (e: Exception) {
+                // Handle network or other exceptions
             }
         }
 
---
+++ FrontendSafetyFunMap/app/src/main/java/com/example/frontendsafetyfunmap/PostModel.kt
@@ -0,0 +1,7 @@
+package com.example.frontendsafetyfunmap
+
+data class PostModel(
+    val id: String,
+    val username: String,
+    val text: String
+)
