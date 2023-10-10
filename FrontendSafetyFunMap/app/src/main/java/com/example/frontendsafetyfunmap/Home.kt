f93a70fb051e4ebd5b442fa349d3219e40f5250f
AndreiNoroc <noroc_andrei@yahoo.com>
2023-10-10 03:46:35 +0300

    try to display posts

--- FrontendSafetyFunMap/app/src/main/java/com/example/frontendsafetyfunmap/Home.kt
+++ FrontendSafetyFunMap/app/src/main/java/com/example/frontendsafetyfunmap/Home.kt
@@ -2,14 +2,19 @@ package com.example.frontendsafetyfunmap
 
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.json.JSONArray
 
 import retrofit2.Call
 import retrofit2.http.GET
@@ -20,6 +25,20 @@ import retrofit2.await
 interface ApiService {
     @GET("/posts/")
     fun getAllPosts(): Call<List<PostModel>>
+}
+
+suspend fun getApiResponse(): List<PostModel> {
+    try {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://192.168.100.20:8080")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = retrofit.create(ApiService::class.java)
+        return apiService.getAllPosts().await()
+    } catch (e: Exception) {
+        throw e
+    }
 }
 
 class Home: Fragment() {
@@ -34,19 +53,35 @@ class Home: Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        val txtViewHome = view.findViewById<TextView>(R.id.textViewHome)
+        val scrollViewHome = view.findViewById<ScrollView>(R.id.scrollViewHome)
+        val linearContainer = scrollViewHome.findViewById<LinearLayout>(R.id.linearContainer)
+//        linearContainer.removeAllViews()
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("http://192.168.100.20:8080")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val apiService = retrofit.create(ApiService::class.java)
         CoroutineScope(Dispatchers.IO).launch {
             try {
-                val response = apiService.getAllPosts().await()
+                val response = getApiResponse()
+                Log.d("start", response.toString())
                 if (response.isNotEmpty()) {
-                    txtViewHome.text = response[0].username
+                    response.forEach {
+                        val post = it
+                        Log.d("svfservf", post.username)
+
+                        val cardView = CardView(linearContainer.context)
+                        val layoutParams = LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            500
+                        )
+                        cardView.layoutParams = layoutParams
+                        cardView.cardElevation = 4f
+                        cardView.radius = 8f
+
+                        val textView = TextView(cardView.context)
+                        textView.text = post.username
+                        textView.textSize = 70.0F
+                        cardView.addView(textView)
+
+                        linearContainer.addView(cardView)
+                    }
                 } else {
                     // Handle an error response (non-2xx status code)
                 }
